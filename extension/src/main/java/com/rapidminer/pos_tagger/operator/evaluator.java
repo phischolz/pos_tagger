fc324e27fea97423b6c6adf58f62fe44de08e0ae
phischolz <33486489+phischolz@users.noreply.github.com>
2018-11-19 11:49:36 +0100

    prepped evaluator

--- extension/src/main/java/com/rapidminer/pos_tagger/operator/evaluator.java
+++ extension/src/main/java/com/rapidminer/pos_tagger/operator/evaluator.java
@@ -1,11 +1,15 @@
 package com.rapidminer.pos_tagger.operator;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 
 import com.rapidminer.example.Attributes;
 import com.rapidminer.example.Example;
 import com.rapidminer.example.ExampleSet;
 import com.rapidminer.example.table.AttributeFactory;
+import com.rapidminer.example.table.ExampleTable;
+import com.rapidminer.example.table.MemoryExampleTable;
 import com.rapidminer.operator.Operator;
 import com.rapidminer.operator.OperatorDescription;
 import com.rapidminer.operator.OperatorException;
@@ -18,8 +22,10 @@ import javax.xml.stream.events.Attribute;
 
 
 public class evaluator extends Operator{
-    private InputPort exampleSetInput = getInputPorts().createPort("in 1");
-    private OutputPort exampleSetOutput = getOutputPorts().createPort("out 1");
+    private InputPort GoldInput = getInputPorts().createPort("Gold In");
+    private InputPort ResultInput = getInputPorts().createPort("Result In");
+    private OutputPort EvalOutput = getOutputPorts().createPort("Eval");
+    //TODO Limitless Ports (Result [x] in)?
 
     public evaluator(OperatorDescription description) {
 
@@ -28,24 +34,32 @@ public class evaluator extends Operator{
 
     @Override
     public void doWork() throws OperatorException {
-        //read in
-        ExampleSet exampleSet = exampleSetInput.getData(ExampleSet.class);
-        Attributes attributes=exampleSet.getAttributes();
-        String newN = "zweiundvierzig";
-        //create new Attr
-        com.rapidminer.example.Attribute targetAttribute = AttributeFactory.createAttribute(newN, Ontology.INTEGER);
-        targetAttribute.setTableIndex(attributes.size());
-        exampleSet.getExampleTable().addAttribute(targetAttribute);
-        attributes.addRegular(targetAttribute);
-        //insert random values in Set
-        for(Example example:exampleSet){
-            example.setValue(targetAttribute, Math.round(Math.random()*10+0.5));
-        }
-        //def Table index of new attr
-
+        //TODO read in Gold object
+    	//TODO read in other objects
+        //ExampleSet exampleSet = exampleSetInput.getData(ExampleSet.class);
+        //TODO convert back from IOObj Into Matrix/List Objects
+        
+        //TODO EVALUATE PRECISION
+    	
+    	//TODO Eval-ExampleSet Creation
+    	List<com.rapidminer.example.Attribute> listOfAtts = new LinkedList<>();
+        ExampleSet result;
+        com.rapidminer.example.Attribute nameColumn = AttributeFactory.createAttribute("Input Identifier", 
+                Ontology.ATTRIBUTE_VALUE_TYPE.STRING);
+        listOfAtts.add(nameColumn);
+        com.rapidminer.example.Attribute precisionColumn = AttributeFactory.createAttribute("Precision",
+                Ontology.ATTRIBUTE_VALUE_TYPE.NUMERICAL);
+        listOfAtts.add(precisionColumn);
+        
+        ExampleTable table = new MemoryExampleTable(listOfAtts);
+        
+        
+        //TODO Fill table with Eval Results. Pairs of [INPUT NAME] and corresponding [PRECISION]. Gold has 100% per default.
+        
         LogService.getRoot().log(Level.INFO, "Doing something...");
+        result=table.createExampleSet();
         // output finished Set
-        exampleSetOutput.deliver(exampleSet);
+        EvalOutput.deliver(result);
     }
 
 }
